266f5edff2eb5f730090abf0f55105fb8ae60cb8
cosmin-oros <oroscosmin09@gmail.com>
2022-07-18 20:22:17 +0300

    detail screen

--- app/src/main/java/com/example/mvvmtodoapp/data/Todo.kt
+++ app/src/main/java/com/example/mvvmtodoapp/data/Todo.kt
@@ -5,9 +5,9 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class Todo(
-    @PrimaryKey(autoGenerate = true)
-    val id: Long,
     val todo: String,
     val time: String,
-    val isComplete: Boolean
+    val isComplete: Boolean = false,
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0
 )
