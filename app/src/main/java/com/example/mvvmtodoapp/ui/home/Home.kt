7bec7899bd35deb1a94c4e6f244151a222782682
cosmin-oros <oroscosmin09@gmail.com>
2022-07-18 20:10:28 +0300

    home screen

--- app/src/main/java/com/example/mvvmtodoapp/ui/home/Home.kt
+++ app/src/main/java/com/example/mvvmtodoapp/ui/home/Home.kt
@@ -1,2 +1,41 @@
 package com.example.mvvmtodoapp.ui.home
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mvvmtodoapp.data.Todo
+import com.example.mvvmtodoapp.ui.home.components.TodoItem
+
+@Composable
+fun HomeScreen(onNavigate: (Todo?) -> Unit) {
+    val viewModel = viewModel(HomeViewModel::class.java)
+    val state by viewModel.state.collectAsState()
+
+    Scaffold(floatingActionButton = {
+        FloatingActionButton(onClick = { onNavigate(null) }) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = null)
+        }
+    }) {
+        LazyColumn {
+            items(state.todoList) { todo ->
+                TodoItem(
+                    todo = todo,
+                    onChecked = { viewModel.updateTodo(it, todo.id) },
+                    onDelete = { viewModel.deleteTodo(it) },
+                    onNavigation = { onNavigate(it) }
+                )
+            }
+        }
+
+    }
+
+
+}
